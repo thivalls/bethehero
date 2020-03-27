00365c2b21dd0e26e07a8e96ec69c408277e6816
valls <thiago@agenciatw.com.br>
2020-03-27 00:52:37 -0300

    Ending app structure

--- mobile/src/pages/Detail/index.js
+++ mobile/src/pages/Detail/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Feather } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
-import { View, Text, Image, TouchableOpacity } from 'react-native';
+import { View, Text, Image, TouchableOpacity, Linking } from 'react-native';
+import * as MailComposer from 'expo-mail-composer';
 
 import logoImg from './../../assets/logo.png';
 
@@ -9,9 +10,22 @@ import styles from './style.js';
 
 export default function Detail() {
   const navigation = useNavigation();
+  const message = 'Email message body';
 
   function navigateToListBack() {
     navigation.goBack();
+  }
+
+  function sendMail() {
+    MailComposer.composeAsync({
+      subject: 'Herói do caso: Cadelinha atropelada',
+      recipients: ['olavalls@gmail.com'],
+      body: message,
+    });
+  }
+
+  function sendWhatsapp() {
+    Linking.openURL(`whatsapp://send?phone=5516991597346&text=${message}`);
   }
 
   return (
@@ -31,7 +45,7 @@ export default function Detail() {
       <View style={styles.incident}>
         <View style={styles.viewGroup}>
           <View style={styles.viewIntoGroup}>
-            <Text style={[styles.incidentProperty, {marginTop: 0}]}>
+            <Text style={[styles.incidentProperty, { marginTop: 0 }]}>
               CASO:
             </Text>
             <Text style={styles.incidentValue}>
@@ -39,7 +53,7 @@ export default function Detail() {
             </Text>
           </View>
           <View style={styles.viewIntoGroup}>
-            <Text style={[styles.incidentProperty, {marginTop: 0}]}>
+            <Text style={[styles.incidentProperty, { marginTop: 0 }]}>
               ONG:
             </Text>
             <Text style={styles.incidentValue}>
@@ -51,7 +65,7 @@ export default function Detail() {
           Descrição
         </Text>
         <Text style={styles.incidentValue}>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab dolore deserunt earum dignissimos. Sint iste quam corporis dicta nesciunt? Nesciunt optio nobis est recusandae iste quae esse autem natus distinctio!
+          Lorem ipsum dolor sit, amet consectetur adipisicing
         </Text>
         <Text style={styles.incidentProperty}>
           VALOR
@@ -68,12 +82,12 @@ export default function Detail() {
         <Text style={styles.heroDescription}>Entre em contato:</Text>
 
         <View style={styles.actions}>
-          <TouchableOpacity style={styles.action} onPress={() => { }}>
+          <TouchableOpacity style={styles.action} onPress={sendWhatsapp}>
             <Text style={styles.actionText}>
               WhatsApp
             </Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.action} onPress={() => { }}>
+          <TouchableOpacity style={styles.action} onPress={sendMail}>
             <Text style={styles.actionText}>
               E-mail
             </Text>
