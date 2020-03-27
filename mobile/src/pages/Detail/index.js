534267e7f7d448faf51ded67caf918d211b69676
valls <thiago@agenciatw.com.br>
2020-03-26 23:41:02 -0300

    Creating first layout and navigation

---
+++ mobile/src/pages/Detail/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+import './style.js';
+
+export default function Detail() {
+  return (
+    <View>
+      <Text>Hello DeTAILS</Text>
+    </View>
+  );
+}
