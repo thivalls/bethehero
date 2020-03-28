a4c2e3a57d62b7fdd6407b06c8cd87260ad8c19b
valls <thiago@agenciatw.com.br>
2020-03-28 02:47:51 -0300

    Starting with tests TDD

---
+++ backend/tests/unit/ong.spec.js
@@ -0,0 +1,30 @@
+const request = require('supertest');
+const app = require('./../../src/app');
+const connection = require('./../../src/database/connection');
+
+describe('Ong', () => {
+  beforeEach(async () => {
+    await connection.migrate.latest();
+  });
+
+  afterAll(async () => {
+    await connection.migrate.rollback();
+    await connection.destroy();
+  });
+
+  it('should create a new ong', async () => {
+    const response = await request(app)
+      .post('/ongs')
+      .send({
+        name: "teste",
+        email: "teste@teste.com.br",
+        whatsapp: "16991597346",
+        city: "Ibaté",
+        uf: "SP",
+      });
+
+      expect(response.body).toHaveProperty('id');
+      expect(response.body.id).toHaveLength(8);
+  });
+});
+
